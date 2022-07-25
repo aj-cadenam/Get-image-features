00a51ff382be666643160ab4020c01f155f55583
Andres Cadena <aj-cadena@outlook.com>
2022-07-25 07:46:54 -0500

    Conexion con github

---
+++ Coordenadas.py
@@ -0,0 +1,25 @@
+
+import pyautogui, sys
+
+#p=pyautogui.position()
+#print(p)
+
+"""print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\n')"""
+
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\n')
